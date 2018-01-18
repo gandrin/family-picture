878838207934f245bdac5f83255d390bc54f0d26
gabriel andrin <gabriela@theodo.fr>
2018-01-19 00:32:03 +0100

    Add root page

---
+++ frontend/src/Pages/root/root.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './root.component';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Root />, div);
+});

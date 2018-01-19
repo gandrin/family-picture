c7c54fc8d1a244739bce061c7a9d68775a634301
gabriel andrin <gabriela@theodo.fr>
2018-01-19 10:32:08 +0100

    Add level page

---
+++ frontend/src/Pages/level/level.component.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './level.style.css';
+
+import Puzzle from '../../Components/level';
+
+const levelConfiguration = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+export default class Level extends Component {
+  render() {
+    return (
+      <div className="Level">
+        <Puzzle level={levelConfiguration} />
+      </div>
+    );
+  }
+}
